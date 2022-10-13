1b9dbb853f8f5300b95142632d99af11c5a09b63
Julian NDENDE <julian.ndende@epitech.eu>
2022-10-13 15:32:53 +0200

    [EDIT] : css for SearchTown input component

--- nuxt-app/components/search-town.vue
+++ nuxt-app/components/search-town.vue
@@ -4,7 +4,7 @@
       type="text"
       ref="streetRef"
       placeholder="Recherchez une ville..."
-      class="block mt-1 w-full text-sm placeholder-gray-400 rounded-md border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+      class="search-click mt-1 w-full text-sm placeholder-gray-400 rounded-md border-gray-300 focus:ring-blue-500 focus:border-blue-500"
     />
   </label>
 </template>
