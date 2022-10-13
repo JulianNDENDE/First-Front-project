bb7bd63c6473ef9fb83a0f246cf2121439403cc1
Julian NDENDE <julian.ndende@epitech.eu>
2022-10-13 11:17:00 +0200

    [EDIT] : css for email-input navbar, now it's not applied on every input field

--- nuxt-app/components/email-input.vue
+++ nuxt-app/components/email-input.vue
@@ -1,11 +1,13 @@
 <template>
   <input
     type="email"
-    class="search-click container mx-auto my-2 p-2"
+    class="search-click check-email container mx-auto my-2 p-2"
     placeholder="Enter your email here..."
     required pattern="^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,})$"
     @keydown.enter="validEmail($event)"
   />
+  <!-- <p class="text-green-500" v-if="validEmail">Valid Email</p>
+  <p class="text-red-500" v-else>Invalid Email</p> -->
 </template>
 
 <script>
@@ -14,6 +16,7 @@ export default {
     validEmail(event) {
       const email = event.target.value;
       const emailRegex = /^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,})$/;
+      if (email == "") return false;
       return emailRegex.test(email);
     },
   },
