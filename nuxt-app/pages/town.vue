3abebad4112e19724e39c0f15d0bf104837c62ed
Julian NDENDE <julian.ndende@epitech.eu>
2022-11-03 20:24:04 +0100

    [ADD] : components that compare the distance between a list of center and return the closest one to the city given in parameter

--- nuxt-app/pages/town.vue
+++ nuxt-app/pages/town.vue
@@ -16,4 +16,10 @@
     <h2 class="font-bold text-black">Town research :</h2>
     <search-town />
   </div>
+
+  <div class="container mx-auto bg-gray-300 p-8 my-8">
+    <h1 class="font-bold text-black text-4xl text-center">Compare distance</h1>
+    <h2 class="font-bold text-black">Town research :</h2>
+    <compare-distance />
+  </div>
 </template>
